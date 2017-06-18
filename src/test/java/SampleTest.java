96aab699061580d921760dfbcfcdce601e474d0c
Mikhail Yakushin <driver733@me.com>
2017-06-19 01:54:31 +0400

    fix qulice

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * Class or Interface description.
+ * <p>
+ * Additional info
+ *
+ * @author Mikhail Yakushin (driver733@me.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SampleTest {
+
+    @Test
+    public void evaluatesExpression() {
+        assertEquals(4, 2+2);
+    }
+
+}

8aedfc6f59cdb4d24eb1738dbb77ef7cbc0c47f9
Mikhail Yakushin <driver733@me.com>
2017-06-22 18:30:38 +0400

    #27 Added required tests

--- src/test/java/com/driver733/vkmusicuploader/wallpost/attachment/support/AttachmentFromResultsTest.java
+++ src/test/java/com/driver733/vkmusicuploader/wallpost/attachment/support/AttachmentFromResultsTest.java
@@ -38,16 +38,15 @@ import org.junit.Test;
  * @author Mikhail Yakushin (driver733@me.com)
  * @version $Id$
  * @since 0.1
- * @todo #3 Need to create tests for all attachment types.
  * @checkstyle JavadocMethodCheck (500 lines)
  * @checkstyle LocalFinalVariableNameCheck (50 lines)
- * @checkstyle StringLiteralsConcatenationCheck (50 lines)
+ * @checkstyle StringLiteralsConcatenationCheck (100 lines)
  * @checkstyle CascadeIndentationCheck (50 lines)
  */
 public final class AttachmentFromResultsTest {
 
     @Test
-    public void statusUploaded() throws IOException {
+    public void statusUploadedAudio() throws IOException {
         MatcherAssert.assertThat(
             new AttachmentsFromResults(
                 new GsonBuilder()
@@ -76,6 +75,58 @@ public final class AttachmentFromResultsTest {
             ).attachments(),
             Matchers.containsInAnyOrder(
                 "audio1111111_1000000", "audio2222222_2000000"
+            )
+        );
+    }
+
+    @Test
+    public void statusUploadedPhoto() throws IOException {
+        MatcherAssert.assertThat(
+            new AttachmentsFromResults(
+                new GsonBuilder()
+                    .setPrettyPrinting()
+                    .create()
+                    .fromJson(
+                        "["
+                            + "{"
+                            + "\"owner_id\"     : 111111,"
+                            + "\"id\"           : 333333,"
+                            + "\"album_id\"     : 121212,"
+                            + "\"user_id\"      : 100,"
+                            + "\"date\"         : 14981391923,"
+                            + "\"text\"         : \"Test description\","
+                            + "\"photo_75\"     : \"http://vk.com/test67567\","
+                            + "\"photo_130\"    : \"http://vk.com/test11223\","
+                            + "\"photo_604\"    : \"http://vk.com/test2323\","
+                            + "\"photo_807\"    : \"http://vk.com/test345344\","
+                            + "\"photo_1280\"   : \"http://vk.com/test34534\","
+                            + "\"photo_2560\"   : \"http://vk.com/test345435\","
+                            + "\"width\"        : 12803,"
+                            + "\"heigth\"       : 10254"
+                            + "},"
+                            + "{"
+                            + "\"owner_id\"     : 222222,"
+                            + "\"id\"           : 444444,"
+                            + "\"album_id\"     : 1212123,"
+                            + "\"user_id\"      : 101,"
+                            + "\"date\"         : 1498139192,"
+                            + "\"text\"         : \"Test description2\","
+                            + "\"photo_75\"     : \"http://vk.com/test34\","
+                            + "\"photo_130\"    : \"http://vk.com/test456\","
+                            + "\"photo_604\"    : \"http://vk.com/test4564\","
+                            + "\"photo_807\"    : \"http://vk.com/test34654\","
+                            + "\"photo_1280\"   : \"http://vk.com/test99\","
+                            + "\"photo_2560\"   : \"http://vk.com/test534656\","
+                            + "\"width\"        : 12801,"
+                            + "\"heigth\"       : 10243"
+                            + "}"
+                            + "]",
+                        JsonArray.class
+                    )
+                    .getAsJsonArray()
+            ).attachments(),
+            Matchers.containsInAnyOrder(
+                "photo111111_333333", "photo222222_444444"
             )
         );
     }

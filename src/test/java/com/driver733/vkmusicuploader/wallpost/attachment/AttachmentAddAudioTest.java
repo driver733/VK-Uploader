b3effdbedbf9b2447b3668324977eb36a8da40b8
Mikhail Yakushin <driver733@me.com>
2017-06-25 16:24:42 +0400

    #8 Completed

--- src/test/java/com/driver733/vkmusicuploader/wallpost/attachment/AttachmentAddAudioTest.java
+++ src/test/java/com/driver733/vkmusicuploader/wallpost/attachment/AttachmentAddAudioTest.java
@@ -45,9 +45,9 @@ import org.junit.Test;
  * @author Mikhail Yakushin (driver733@me.com)
  * @version $Id$
  * @since 0.1
+ * @todo #14 Replace group id constant with maven exec param.
  * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
  * @checkstyle JavadocMethodCheck (500 lines)
- * @todo #14 Replace group id constant with maven exec param.
  */
 public final class AttachmentAddAudioTest {
 

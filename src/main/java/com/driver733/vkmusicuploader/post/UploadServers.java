d999905a93b4ed564cb5642807bb96ea8b59cf63
Mikhail Yakushin <driver733@me.com>
2017-06-26 20:07:40 +0400

    #44 Completed

--- src/main/java/com/driver733/vkmusicuploader/post/UploadServers.java
+++ src/main/java/com/driver733/vkmusicuploader/post/UploadServers.java
@@ -39,7 +39,6 @@ import java.io.IOException;
  * @author Mikhail Yakushin (driver733@me.com)
  * @version $Id$
  * @since 0.1
- * @todo #12 Create applicable tests for this class.
  */
 @Immutable
 public final class UploadServers {
